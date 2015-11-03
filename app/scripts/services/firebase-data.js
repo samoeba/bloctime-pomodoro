980731588d9f142ac3ceb0e7254d56baf87f5f0c
Sam Casey <samcaseydesign@gmail.com>
2015-11-03 10:01:54 -0600

    trying to integrate firebase data - not working

--- app/scripts/services/firebase-data.js
+++ app/scripts/services/firebase-data.js
@@ -1,9 +1,11 @@
 var pomodoroServices = angular.module("services", []);
 
-pomodoroServices.factory("Tasks", ["$firebaseArray", "$scope", function($firebaseArray, $scope) {
+pomodoroServices.factory("Tasks", ["$firebaseArray", function($firebaseArray) {
+    "use strict";
 
-    var ref = new Firebase("https://brilliant-torch-364.firebaseio.com/");
-    var tasks = $firebaseArray(ref);
+    var ref, tasks;
+    ref = new Firebase("https://brilliant-torch-364.firebaseio.com/");
+    tasks = $firebaseArray(ref);
 
     return {
         all: tasks
--- dist/scripts/controllers/main-controller.js
+++ dist/scripts/controllers/main-controller.js
@@ -1,10 +1,11 @@
 angular.module("blocTime")
 
-    .controller("MainCtrl", ["$scope", function($scope) {
+    .controller("MainCtrl", ["$scope", "Tasks", function($scope, Tasks) {
         "use strict";
 
         $scope.test = "Can you hear me now?";
 
+        $scope.completedTasks = Tasks.all;
 
     }]);
 
