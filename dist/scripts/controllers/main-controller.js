d418d9591991a48fd0aa57b28b2f486d81293379
Sam Casey <samcaseydesign@gmail.com>
2015-11-19 01:03:55 -0600

    finished filter tabs

--- dist/scripts/controllers/main-controller.js
+++ dist/scripts/controllers/main-controller.js
@@ -6,14 +6,15 @@ angular.module("blocTime")
     .controller("MainCtrl", ["$scope", "Tasks", function($scope, Tasks) {
         "use strict";
 
-        $scope.newTask = { title: "", created: "", pomosEst: "", pomosComp: "0", /*isSelected: "",*/ isCompleted: "false" };
+        $scope.newTask = { title: "", created: "", pomosEst: "", pomosComp: "0", /*isSelected: "",*/ isCompleted: false };
         $scope.currentTask = null;
+        $scope.tab = "todo";
         $scope.completedTasks = Tasks.all;
 
         $scope.addTask = function () {
             $scope.newTask.created = new Date().getTime();
             Tasks.addTask(angular.copy($scope.newTask));
-            $scope.newTask = { title: "", created: "", pomosEst: "", pomosComp: "0", /*isSelected: "",*/ isCompleted: "false" };
+            $scope.newTask = { title: "", created: "", pomosEst: "", pomosComp: "0", /*isSelected: "",*/ isCompleted: false };
         };
 
         $scope.removeTask = function (task, event) {
@@ -23,6 +24,9 @@ angular.module("blocTime")
         };
 
         $scope.selectTask = function (task) {
+            if (task.isCompleted) {
+                return;
+            }
             if (task === $scope.currentTask) {
                 task.isSelected = false;
                 $scope.currentTask = null;
@@ -37,22 +41,30 @@ angular.module("blocTime")
             //$scope.completedTasks.$save(task);
         };
 
-        var allTasks = document.querySelectorAll(".task-item");
+        var allTasks = document.getElementsByClassName("task-item");
 
         $scope.showCompleted = function () {
             console.log("Showing completed");
             console.log(allTasks);
             for (var i = 0; i < allTasks.length; i++) {
-                allTasks[i].setAttribute("ng-show", "task.isCompleted === true")
+                allTasks[i].setAttribute("ng-show", "task.isCompleted === true");
+                //$scope.apply();
             }
         };
 
         $scope.showTodos = function () {
             console.log("Showing todos");
             for (var i = 0; i < allTasks.length; i++) {
-                allTasks[i].setAttribute("ng-show", "task.isCompleted === 'false'")
+                allTasks[i].setAttribute("ng-show", "task.isCompleted === 'false'");
             }
         };
+
+        var counter = 0;
+
+        $scope.$watch(function () {
+            counter += 1;
+            console.log(counter);
+        })
 
     }]);
 
